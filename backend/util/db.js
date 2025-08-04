b010806b4992c9c8ae2a6118fe22f8eda20e1c0b
mario <mario85.girges@gmail.com>
2025-08-04 21:45:10 +0200

    s s

--- backend/util/db.js
+++ backend/util/db.js
@@ -3,12 +3,12 @@ const Sequelize = require("sequelize");
 
 // Use environment variables for database configuration
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "dragon",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   }
 );
