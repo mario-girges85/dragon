c6e1a0aab9758e2338000d90f3f44c4c631fdfe5
mario <mario85.girges@gmail.com>
2025-08-04 21:34:11 +0200

    dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // backend/index.js
 
 // Load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
@@ -25,7 +25,7 @@ const usersRouter = require("./routes/users");
 const orderRouter = require("./routes/order");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // --- Core Middleware ---
 app.use(
